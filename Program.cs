30af827e3f0a9bba21102de683a7f914e095cd30
Meenaakshi-Ambati <ambatimeenakshi9@gmail.com>
2021-08-23 22:26:11 +0530

    Employee class Defined

--- Program.cs
+++ Program.cs
@@ -2,11 +2,28 @@
 
 namespace HelloWorld
 {
+    class Employee
+        {
+            public int Id  {get; set; }
+            public string Name { get; set; }
+
+            public double Salary { get; set; }
+
+
+            public  override string ToString()
+            {
+                return  $"{Id} - {Name} - {Salary}";
+            }
+        }
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World Updated!");
+            
+            Employee employee = new Employee{ Id = 101, Name = "Meenaa", Salary = 80000 };
+            Console.WriteLine(employee);
         }
     }
 }
